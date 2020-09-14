048083cae1d47024258bbe9dcc34ca9ddca82bff
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-14 17:07:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,3 +1,4 @@
+=begin
 def prime?(num)
   if num <= 1 
     false 
@@ -6,6 +7,19 @@ def prime?(num)
   elsif num.even?
     false
   elsif num > 2 && (2..num-1).find{|i| num % i == 0}
+    false 
+  else 
+    true
+  end
+end
+=end
+
+def prime?(num)
+  if num <= 1 
+    false 
+  elsif num.even? && num != 2
+    false
+  elsif num.odd? && (2..num-1).find{|i| num % i == 0}
     false 
   else 
     true
