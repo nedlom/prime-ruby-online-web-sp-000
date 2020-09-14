dc9b24ac3eb78b1f004065276c65588916596731
imperative-scientist-4600 <nelom3@gmail.com>
2020-09-14 17:00:38 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,11 @@
-# Add  code here!
+def prime?(num)
+  if num <= 1 
+    false 
+  elsif num == 2
+    true
+  elsif num > 2 && (2..num-1).find{|i| num % i == 0}
+    false 
+  else 
+    true
+  end
+end
