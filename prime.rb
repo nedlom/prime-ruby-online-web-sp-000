40f8eb296e39f29d8339b7640e873f76806c98cf
imperative-scientist-4600 <nelom3@gmail.com>
2020-10-15 22:06:13 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,27 +1,28 @@
-=begin
 def prime?(num)
-  if num <= 1 
-    false 
-  elsif num == 2
-    true
-  elsif num.even?
-    false
-  elsif num > 2 && (2..num-1).find{|i| num % i == 0}
-    false 
-  else 
-    true
-  end
+  num > 1 && (2..num-1).to_a.none?{|i| num % i == 0}
 end
-=end
-
-def prime?(num)
-  if num <= 1 
-    false 
-  elsif num.even? && num != 2
-    false
-  elsif num.odd? && (2..num-1).find{|i| num % i == 0}
-    false 
-  else 
-    true
-  end
-end
+  
+# def factors?(num)
+#   i = 2
+#   prime = false
+#   while i < num
+#     if num % i == 0
+#       prime = false
+#       break
+#     end
+#     prime = true
+#     i += 1
+#   end  
+#   prime
+# end  
+  
+#   if num <= 1 
+#     false 
+#   elsif num.even? && num != 2
+#     false
+#   elsif num.odd? && (2..num-1).find{|i| num % i == 0}
+#     false 
+#   else 
+#     true
+#   end
+# end
