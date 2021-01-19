bedf2cd518970cdcb71c45b12cf51bae83947017
imperative-scientist-4600 <nelom3@gmail.com>
2021-01-19 23:14:26 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,9 @@
 def prime?(num)
-  num > 1 && (2..num-1).to_a.none?{|i| num % i == 0}
+  # (2..num).to_a.select{|x| num % x == 0} == [num]
+  #num > 1 && (2..num-1).to_a.none?{|i| num % i == 0}
+  y = [2] + (3..num).step(2).to_a
+    y.detect {|n| num % n == 0} == num
+    
 end
   
 # def factors?(num)
