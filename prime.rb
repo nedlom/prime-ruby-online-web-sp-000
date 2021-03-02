f9dd5b431074ef1b2c08d24801804621b414ffa1
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-02 21:22:55 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,32 +1,7 @@
+require 'benchmark'
+require 'pry'
+
 def prime?(num)
-  # (2..num).to_a.select{|x| num % x == 0} == [num]
-  #num > 1 && (2..num-1).to_a.none?{|i| num % i == 0}
   y = [2] + (3..num).step(2).to_a
-    y.detect {|n| num % n == 0} == num
-    
+  y.detect {|n| num % n == 0} == num
 end
-  
-# def factors?(num)
-#   i = 2
-#   prime = false
-#   while i < num
-#     if num % i == 0
-#       prime = false
-#       break
-#     end
-#     prime = true
-#     i += 1
-#   end  
-#   prime
-# end  
-  
-#   if num <= 1 
-#     false 
-#   elsif num.even? && num != 2
-#     false
-#   elsif num.odd? && (2..num-1).find{|i| num % i == 0}
-#     false 
-#   else 
-#     true
-#   end
-# end
