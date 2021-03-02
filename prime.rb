8badc6850a3c58d9c0abbd4fe0387a1ee2bc3197
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-02 21:43:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,13 @@
 require 'benchmark'
 require 'pry'
 
-def prime?(num)
-  y = [2] + (3..num).step(2).to_a
-  y.detect {|n| num % n == 0} == num
-end
+# def prime?(num)
+#   y = [2] + (3..num).step(2).to_a
+#   y.detect {|n| num % n == 0} == num
+# end
 
 def prime?(num)
   x = (2..num).to_a
-  y = x.
+  y = x.reject {|n| n**n > num}
+  binding.pry
 end
