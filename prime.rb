f2afbde801507aac0e42c40423ba6f16c8af1e46
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-02 21:56:03 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -9,13 +9,9 @@ require 'pry'
 def prime?(num)
   num = num - 1
   x = (2..num).to_a
-  binding.pry
   y = x.reject {|n| n**2 > num}
-  binding.pry
   z = y.reject {|n| n.even?}
-  binding.pry
   a = z.select {|n| num % n == 0}
-  binding.pry
   num == 2 || a.empty?
 end
 
