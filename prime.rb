9e1178d2e7fc97ae03e49c019ab141d95729f39d
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-02 22:21:57 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -5,3 +5,16 @@ def prime?(num)
   y = [2] + (3..num).step(2).to_a
   y.detect {|n| num % n == 0} == num
 end
+
+def prime1?(num)
+  array = []
+  n = 1
+  while n ** 2 <= num
+    if num % n == 0
+      array << n
+    end
+    n += 1
+  end
+  array.length == 1 && num != 1
+end
+binding.pry
