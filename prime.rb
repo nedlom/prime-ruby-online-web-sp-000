5e15a25636eb1fc237d4632f54ebdd717edd9882
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-02 21:50:15 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -7,10 +7,12 @@ require 'pry'
 # end
 
 def prime?(num)
-  x = (2..num).to_a
+  num = num - 1
+  x = (2..num - 1).to_a
   y = x.reject {|n| n**2 > num}
   z = y.reject {|n| n.even? if n > 2}
   a = z.select {|n| num % n == 0}
+  b = a.reject {
   !a.empty?
 end
 
